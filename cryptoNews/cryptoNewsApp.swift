e6670df27fb3e2d299d2ab9f35151b5a51c6998f
trungnam <hjtman9922@gmail.com>
2024-07-01 12:28:16 +0700

    Initial Commit

---
+++ cryptoNews/cryptoNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoNewsApp.swift
+//  cryptoNews
+//
+//  Created by trungnam on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct cryptoNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
